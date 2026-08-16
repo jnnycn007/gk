90920f4bc32d7d4eebfb857425c98e295e558456
John Cronin <jncronin@tysos.org>
2026-08-16 13:15:12 +0100

    add support for ET_REL images

--- Firmware-v4/flash_image_maker/flash_image_maker.py
+++ Firmware-v4/flash_image_maker/flash_image_maker.py
@@ -18,17 +18,36 @@ for ifile in args.input_files:
     with open(ifile, "rb") as f:
         elffile = ELFFile(f)
 
-        for segment in elffile.iter_segments():
-            if segment['p_type'] == 'PT_LOAD':
-                data = segment.data()
-                paddr = segment['p_paddr']
-                flash_offset = paddr - FLASH_BASE
+        if elffile['e_type'] == 'ET_EXEC':
+            for segment in elffile.iter_segments():
+                if segment['p_type'] == 'PT_LOAD':
+                    data = segment.data()
+                    paddr = segment['p_paddr']
+                    flash_offset = paddr - FLASH_BASE
 
-                if flash_offset < 0 or (flash_offset + len(data)) > args.output_size:
-                    print("Segment outside flash: {hex(paddr)} - {hex(paddr + len(data))}")
-                    sys.exit(1)
-                
-                flash_buffer[flash_offset:flash_offset + len(data)] = data
+                    if flash_offset < 0 or (flash_offset + len(data)) > args.output_size:
+                        print(f"Segment outside flash: {hex(paddr)} - {hex(paddr + len(data))}")
+                        sys.exit(1)
+                    
+                    flash_buffer[flash_offset:flash_offset + len(data)] = data
+
+                    print(f"{str(ifile)}: PT_LOAD: {hex(paddr)} - {hex(paddr + len(data))}")
+                    
+        if elffile['e_type'] == 'ET_REL':
+            for section in elffile.iter_sections():
+                if section['sh_type'] == 'SHT_PROGBITS':
+                    data = section.data()
+                    paddr = section['sh_addr']
+
+                    flash_offset = paddr - FLASH_BASE
+
+                    if flash_offset < 0 or (flash_offset + len(data)) > args.output_size:
+                        print(f"Section outside flash: {hex(paddr)} - {hex(paddr + len(data))}")
+                        sys.exit(1)
+                    
+                    flash_buffer[flash_offset:flash_offset + len(data)] = data
+
+                    print(f"{str(ifile)}: SHT_PROGBITS: {hex(paddr)} - {hex(paddr + len(data))}")
 
 with open(args.output_file, "wb") as f:
     f.write(flash_buffer)
